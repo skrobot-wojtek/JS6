3586161a21fb552584c5947b04e0357ad05d7eed
Wojtek Skrobot <97315300+skrobot-wojtek@users.noreply.github.com>
2023-12-11 23:56:36 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,19 +1,29 @@
-let size = 10;
+let size = 15;
 let orderElement = 1;
 const init = () => {
     // kod inicjujący 
     const btn = document.createElement("button");
+    const reset = document.createElement("button");
     btn.innerText = "Stwórz liste";
+    reset.innerText = "Reste";
     document.body.appendChild(btn);
-    btn.addEventListener("click", createLiElements)
+    document.body.appendChild(reset);
+    btn.addEventListener("click", createLiElements);
+    reset.addEventListener("click", resetuje);
+}
+const resetuje = () => {
+    const doUkrycia = document.querySelectorAll("li");
+    doUkrycia.forEach(item => {
+        item.textContent = "";
+    });
 }
 const createLiElements = () => {
 
     for (let i = 1; i <= 10; i++) {
         const liel = document.createElement("li");
         liel.innerText = "Element nr" + orderElement;
-        liel.style.fontSize = size+"px";
-        liel.style.marginLeft = 50+"px";
+        liel.style.fontSize = size + "px";
+        liel.style.marginLeft = 50 + "px";
         size++;
         orderElement++
         document.body.appendChild(liel);
